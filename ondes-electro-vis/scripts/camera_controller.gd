9815e6ebdbc0100d2acea72d0f83746456ce324f
Ludovic Garon <ludovicgaron54@gmail.com>
2024-11-12 12:15:24 -0500

    added touchscreen support

--- ondes-electro-vis/scripts/camera_controller.gd
+++ ondes-electro-vis/scripts/camera_controller.gd
@@ -16,10 +16,9 @@ func _ready():
 func _input(event):
 	if event is InputEventMouseButton:
 		if event.pressed:
-			if event.button_index == MOUSE_BUTTON_RIGHT:
+			if event.button_index == MOUSE_BUTTON_LEFT:
 				Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 				rightMousePressed = true
-				print("Right mouse button")
 			if rightMousePressed:
 				if event.button_index == MOUSE_BUTTON_WHEEL_UP:
 					print("Scroll wheel up", event.factor)
@@ -27,14 +26,13 @@ func _input(event):
 				if event.button_index == MOUSE_BUTTON_WHEEL_DOWN:
 					print("Scroll wheel down", event.factor)
 					cam.position.z += ZOOM_SPEED * event.factor
-		elif !event.pressed and event.button_index == MOUSE_BUTTON_RIGHT:
+		elif !event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
 			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 			rightMousePressed = false
 			print("Released Right mouse button")
 	elif event is InputEventMouseMotion and rightMousePressed:
-		print(event.relative)
 		vertical.rotate_x(-event.relative.y * ROTATION_SPEED)
-		rotate_y(event.relative.x * ROTATION_SPEED)
+		rotate_y(-event.relative.x * ROTATION_SPEED)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
